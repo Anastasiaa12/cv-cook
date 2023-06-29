e4499481ec618fce44e91968c5d4fbf7378ae5e3
Anastasiaa12 <88934760+Anastasiaa12@users.noreply.github.com>
2023-06-29 23:56:08 +0300

    init: add change in gallery

--- main.js
+++ main.js
@@ -1,3 +1,34 @@
+// swiper
+// const swiper = new Swiper(".mySwiper", {
+//   cssMode: true,
+//   navigation: {
+//     nextEl: ".swiper-button-next",
+//     prevEl: ".swiper-button-prev",
+//   },
+//   pagination: {
+//     el: ".swiper-pagination",
+//   },
+//   mousewheel: true,
+//   keyboard: true,
+// });
+// const swiper = new Swiper(".swiper", {
+//   effect: "fade",
+//   pagination: {
+//     el: ".swiper-pagination",
+//   },
+//   autoplay: {
+//     delay: 3000,
+//     disableOnInteraction: false,
+//   },
+// });
+$(".gallery-list").magnificPopup({
+  delegate: "a",
+  type: "image",
+  gallery: {
+    enabled: true,
+  },
+});
+
 document.querySelectorAll(".img-container img").forEach((img) => {
   img.onclick = () => {
     document.querySelector(".pop-up").style.display = "block";
